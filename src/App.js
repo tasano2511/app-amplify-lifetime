545bef90f3d3b980e189cd3d86b5f2f1319b4d6f
sushi314@hotmail.com <sushi314@hotmail.com>
2022-02-08 22:01:57 -0500

    added auth, api, and db

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>My Notes App</h1>
+      <h1>Match Schedule</h1>
       <input
         onChange={e => setFormData({ ...formData, 'name': e.target.value})}
         placeholder="Note name"
